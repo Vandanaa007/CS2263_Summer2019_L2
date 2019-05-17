3a3fe89ee946081d5670f74ccd98c7bb76df4ffd
Vandanaa Poteeram <vpoteera@id415m39.cs.unb.ca>
2019-05-17 15:19:13 -0300

    Changes have been made to the ArraySort.c and Makefile!

---
+++ CS2263_Summer2019_L2/ArraySort.c
@@ -0,0 +1,97 @@
+/********************************
+ * 
+ * ArraySort.c
+ * 
+ * Created by Jean-Philippe Legault
+ * 
+ * Your task is to implement in place sorting using the two available functions
+ * swapAdjacent, and compareAdjacent.
+ * 
+ * Some bug might have been introduced... you will have to find out if there are any!
+ * if so, you will have to correct it
+ * 
+ ********************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void printArray(int *array, int size)
+{
+    for(int i=0; i<size; i++)
+    {
+        if(i != 0)
+        {
+            printf(", ");
+        }
+        printf("%d", array[i]);
+    }
+    printf("\n");
+}
+
+void swapAdjacent(int *a, int index)
+{    
+    int temp = *(a + index);
+    *(a + index) = *(a + index + 1);    
+    *(a + index + 1) = temp;
+}
+
+int compareAdjacent(int *a, int index)
+{
+    return *(a + index) - *(a + index + 1);
+}
+
+/**
+ * TODO: implement in place sorting on an array
+ * by using the two functions swapAdjacent and compareAdjacent
+ */
+void inPlaceSort(int *a, int index, int size)
+{
+    for(int j = 0; j < size-1; j++){
+        for(int i = 0; i < size-1; i++){
+            while(compareAdjacent(&a[i],index) > 0){
+                swapAdjacent(&a[i],index);
+            }
+        }
+    }
+    
+}
+
+int main(void)
+{
+
+    int array_size = 0;
+    printf("Enter the array size (>0) and the numbers to fill the array with: ");
+    if(!scanf("%d", &array_size))
+    {
+        printf("ERROR. Must enter an integer.\n");
+        return EXIT_FAILURE;
+    }
+    else if(array_size < 1)
+    {
+        printf("ERROR. array size must be at least 1.\n");
+        return EXIT_FAILURE;
+    }
+
+    int a[array_size];
+
+    /**********************
+     * TODO finish parsing the user input to fill the array
+     * 
+     * it should parse user input with scanf to fill the array with values
+     **********************/
+    
+    int input;
+    for (int i = 0; i < array_size; i++){
+        scanf("%d", &input);
+        a[i] = input;  
+    }
+    
+    //printf("=== Array before Sorting = ");
+    printArray(&a[0], array_size);
+
+    inPlaceSort(&a[0], 0, array_size);
+
+    // printf("=== Array after Sorting = ");
+    printArray(&a[0], array_size);
+
+}
